97910440062e5907b3c7cea144d7429528f99e95
radelmann <robadelmann@gmail.com>
2016-01-03 23:04:43 -0800

    Completed all bare minimum requirements

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,12 +1,8 @@
 angular.module('shortly.links', [])
-  .controller('LinksController', function ($scope, Links) {
+  .controller('LinksController', function($scope, Links) {
     $scope.data = {};
-    
 
-    // $scope.getUrls = function($scope) {
-      Links.getAll().then(function (data) {
+    Links.getAll().then(function(data) {
       $scope.data.links = data;
     });
-   // };
-      //load template ???   
   });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,12 +1,10 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function($scope, $location, Links) {
   $scope.link = {};
-  $scope.submitLink = function () {
-    Links.addOne($scope.link);
-    $location.path('/links');
+  $scope.submitLink = function() {
+    Links.addOne($scope.link).then(function(response) {
+      $location.path('/links');
+    });
   };
-
 });
-
-///handle submit
